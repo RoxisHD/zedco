73fcb295f5709886796410690281fb8efbdea9bb
RoxisHD <38810806+RoxisHD@users.noreply.github.com>
2018-05-08 09:56:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("ZedcoGame");
+    bot.user.setGame("Vous aidez");
     console.log("Le bot a bien été connecté");
 });
 
