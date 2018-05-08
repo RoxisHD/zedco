1fb9e3dfe2b65bdbdefb6007032be3aef60b88ea
RoxisHD <38810806+RoxisHD@users.noreply.github.com>
2018-05-08 10:41:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,15 +9,3 @@ bot.on("ready", function() {
 });
 
 bot.login("NDM5NzYxMTQ2MDk5MTM4NTYx.DdI9-A.ycYPxYGRvMnmuVETe7P3uVg47tU");
-
-bot.on("message", message => {
-    if (message.content === "Ping"){
-        message.reply("Pong!");
-        console.log(" Ping Pong");
-    }
-
-    if (message.content === prefix + "z!help"){
-        message.channel.sendMessage("Voici les commandes du bot : z!ping");
-        console.log("Commande Help demandée !");
-    }
-});
