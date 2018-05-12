6108de3e4c813722d0d0fafc3e175a1e03b6659a
RoxisHD <38810806+RoxisHD@users.noreply.github.com>
2018-05-12 11:46:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,22 @@
 const Discord = require("discord.js");
 
 var bot = new Discord.Client();
-var prefix = ("/");
+var prefix = ("z!");
 
 bot.on("ready", function() {
-    bot.user.setGame("Vous aidez");
+    bot.user.setGame("z!help");
     console.log("Le bot a bien été connecté");
 });
 
 bot.login("NDM5NzYxMTQ2MDk5MTM4NTYx.DdI9-A.ycYPxYGRvMnmuVETe7P3uVg47tU");
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+
+    if (msg.content === 'z!help') {
+        msg.reply('Commande du bot : z!help')
+    }
+    
+});
